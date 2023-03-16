48b7cac7c1d50148b38835f8ecb384a3d04d7284
sitauros <sitauros@gmail.com>
2023-03-16 16:02:53 -0400

    Changed regex expression for email check Renamed some variables.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var form_submitted = false;
+var form_submit_in_progress = false;
 
 $(document).ready(function(){
   $("#submit_button").on("click", function(){
@@ -6,7 +6,7 @@ $(document).ready(function(){
   })
  
   hide_form_errors()
-  $("#thanks").hide();
+  $("#thank_you_page").hide();
 })
 
 function hide_form_errors(){
@@ -16,38 +16,38 @@ function hide_form_errors(){
 
 function submit_form(){
  
-  if(!form_submitted){
-   user_email = $("#email").val() 
-   user_interest = $("#interest").val()
-
-   // Clear previous errors, if any
-   hide_form_errors()
-
-   if(!isValidEmailAddress(user_email)){
-     $("#email_error").css('visibility','visible')
-     $("#email").addClass("red_border")
-   }
-   else if (user_interest === null) {
-     $(".interest_error").css('visibility','visible')
-     $("#interest").addClass("red_border")
-   }
-   else{
-    $("#submit_button").html("Submitting...");
-    form_submitted = true;
-    console.log("Email: " + user_email + " | Interest: " + user_interest);
-    
-    // Hide form, show thanks
-    $("#internship_form").delay(2000).hide(0);
-    $("#thanks").delay(2000).show(0);
-   }
+  if(form_submit_in_progress){
+    alert("Please submit the form only once. Press OK to continue.")
+    console.log("Please submit the form only once. Press OK to continue.")
   }
   else{
-    alert("Form has already been submitted. A direct HTML request?! Are you a robot?")
-    console.log("Form has already been submitted. A direct HTML request?! Are you a robot?")
+    user_email = $("#email").val() 
+    user_interest = $("#interest").val()
+
+    // Clear previous errors if present
+    hide_form_errors()
+
+    if(!isValidEmailAddress(user_email)){
+      $("#email_error").css('visibility','visible')
+      $("#email").addClass("red_border")
+    }
+    else if (user_interest === null) {
+      $(".interest_error").css('visibility','visible')
+      $("#interest").addClass("red_border")
+    }
+    else{
+      $("#submit_button").html("Submitting...");
+      form_submit_in_progress = true;
+      console.log("Email: " + user_email + " | Interest: " + user_interest);
+    
+      // Simulate form submission delay, then hide form afterward
+      $("#internship_form").delay(2000).hide(0);
+      $("#thank_you_page").delay(2000).show(0);
+   }
   }
 }
 
-// Very rudimentary email regex
+// Rudimentary email regex check
 function isValidEmailAddress(emailAddress) {
     var pattern = /^[a-z1-9]+@[a-z1-9]+.[a-z1-9]{2,6}$$/i;
     return pattern.test(emailAddress);
